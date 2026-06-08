02756e2f2536d06584e26927e867475f745d2b1a
Chanu Kim <84301139+aries043@users.noreply.github.com>
2026-06-08 22:52:58 +0900

    Added exception handling when creating DisplayManager()

--- display/app/main.py
+++ display/app/main.py
@@ -14,21 +14,26 @@ def setup_buttons():
 
 
 def main():
-    setup_buttons()
-
-    display_manager = DisplayManager()
+    display_manager = None
 
     try:
+        setup_buttons()
+
+        display_manager = DisplayManager()
+
         start_page(display_manager)
-        print("시작 화면 출력 완료& start button pressed")
+        print("시작 화면 출력 완료 & start button pressed")
+
         selected_menu = menu_page(display_manager)
         print(f"선택된 메뉴 버튼: {selected_menu}")
-        
+
     except KeyboardInterrupt:
         print("\n프로그램 종료")
 
     finally:
-        display_manager.cleanup()
+        if display_manager is not None:
+            display_manager.cleanup()
+
         GPIO.cleanup()
 
 
