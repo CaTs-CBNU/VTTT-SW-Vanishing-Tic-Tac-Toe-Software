6f051153571e428bc5d03a6f9847c0fb374fe769
Kim Minseo <128784989+kmingseo@users.noreply.github.com>
2026-06-08 19:44:13 +0900

    update for button_pins

--- app/pin.py
+++ app/pin.py
@@ -6,4 +6,14 @@ import board
 import digitalio
 from adafruit_rgb_display import st7789
 
-# 핀 번호 정의해주세요
+BUTTON_PINS = {
+    1: 17,
+    2: 22,
+    3: 5,
+    4: 6,
+    5: 13,
+    6: 19,
+    7: 20,
+    8: 21,
+    9: 26,
+}
