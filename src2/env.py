ce3f9104eebcb651e293fe63936826c30b35a7db
kimdongkyun <glimbab2002@gmail.com>
2026-06-08 23:30:46 +0900

    Add another method for RL: ddqn(double dqn) - env setting

--- src2/env.py
+++ src2/env.py
@@ -12,3 +12,17 @@ class VanishingTicTacToeEnv:
         self.current_player = 1
         self.done = False
         return self.get_observation()
+    
+    def get_observation(self):
+        obs = np.zeros((2, self.board_size, self.board_size), dtype=np.float32)
+        players = [self.current_player, -self.current_player]
+        
+        for channel_idx, p in enumerate(players):
+            queue = self.pieces[p]
+            for q_idx, (r, c) in enumerate(queue):
+                lifespan = 3 - (len(queue) - 1 - q_idx)
+                obs[channel_idx, r, c] = lifespan
+        return obs
+
+    def get_valid_actions(self):
+        return [i for i in range(9) if self.board[i//3, i%3] == 0]
