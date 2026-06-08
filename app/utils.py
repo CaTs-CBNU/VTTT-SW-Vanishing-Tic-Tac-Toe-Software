8003edbf9158e6df4c6a96b245e564a25b5d9e67
Jiji <luminous037@gmail.com>
2026-06-08 14:42:37 +0900

    add utils.py

---
+++ app/utils.py
@@ -0,0 +1,40 @@
+import time
+import board
+import digitalio
+from adafruit_rgb_display import st7789
+
+from pin import LCD_CS_PINS, DC_PIN, RESET_PIN
+
+class DisplayManager:
+    def __init__(self):
+        self.spi = board.SPI()
+
+        self.dc_pin = digitalio.DigitalInOut(DC_PIN)
+        self.dc_pin.direction = digitalio.Direction.OUTPUT
+
+        self.cs_pins = {}
+        self.displays = {}
+
+        for lcd_num, cs_board_pin in LCD_CS_PINS.items():
+            cs_pin = digitalio.DigitalInOut(cs_board_pin)
+            cs_pin.direction = digitalio.Direction.OUTPUT
+            cs_pin.value = True
+            self.cs_pins[lcd_num] = cs_pin
+
+        self.reset_all_displays()
+
+        for lcd_num, cs_pin in self.cs_pins.items():
+            self.displays[lcd_num] = st7789.ST7789(
+                self.spi,
+                cs=cs_pin,
+                dc=self.dc_pin,
+                rst=None,
+                width=240,
+                height=240,
+                rotation=0,
+                baudrate=1000000,
+                y_offset=80,
+            )
+
+            self.disable_all_cs()
+            time.sleep(0.05)
