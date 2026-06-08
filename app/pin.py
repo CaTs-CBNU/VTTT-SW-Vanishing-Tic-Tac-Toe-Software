4b3fe1966960613c6ac14d80ac7494a58a043a8b
Kim Minseo <128784989+kmingseo@users.noreply.github.com>
2026-06-08 19:47:12 +0900

    update for LCD_cs_pins

--- app/pin.py
+++ app/pin.py
@@ -17,3 +17,15 @@ BUTTON_PINS = {
     8: 21,
     9: 26,
 }
+
+LCD_CS_PINS = {
+    1: board.D2,    # GPIO2,  Pin 3
+    2: board.D15,    # GPI15,  Pin 10
+    3: board.D4,    # GPIO4,  Pin 7
+    4: board.D12,   # GPIO12, Pin 32
+    5: board.D16,   # GPIO16, Pin 36
+    6: board.D18,   # GPIO18, Pin 12
+    7: board.D14,   # GPIO14, Pin 8
+    8: board.D23,   # GPIO23, Pin 16
+    9: board.D24,   # GPIO24, Pin 18
+}
