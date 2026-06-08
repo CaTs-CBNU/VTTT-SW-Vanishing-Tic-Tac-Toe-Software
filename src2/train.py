ca280645390b6a87b59a37c66cadd8ca3ecb7b58
kimdongkyun <glimbab2002@gmail.com>
2026-06-08 23:35:33 +0900

    Add another method for RL: ddqn(double dqn) - training part(train.py)

---
+++ src2/train.py
@@ -0,0 +1,22 @@
+import torch
+import torch.optim as optim
+import torch.nn.functional as F
+import numpy as np
+import random
+import csv
+import copy
+from collections import deque
+from env import VanishingTicTacToeEnv
+from model import DQN
+
+class ReplayBuffer:
+    def __init__(self, capacity=50000):
+        self.buffer = deque(maxlen=capacity)
+    def push(self, state, action, reward, next_state, done):
+        self.buffer.append((state, action, reward, next_state, done))
+    def sample(self, batch_size):
+        batch = random.sample(self.buffer, batch_size)
+        states, actions, rewards, next_states, dones = zip(*batch)
+        return np.array(states), np.array(actions), np.array(rewards), np.array(next_states), np.array(dones)
+    def __len__(self):
+        return len(self.buffer)
