afb48036710dbef2eb2edaaf58848b7f79a283d9
Kim Minseo <128784989+kmingseo@users.noreply.github.com>
2026-06-08 19:54:50 +0900

    update for def show_page

--- app/start.py
+++ app/start.py
@@ -5,3 +5,12 @@ from pin import BUTTON_PINS
 from utils import load_images
 
 START_IMAGE_DIR = Path("./static/images/start")
+
+def show_page(display_manager):
+    start_images = load_images(START_IMAGE_DIR, "start")
+
+    for lcd_num in range(0, 9):
+        display_manager.show_image(
+            lcd_num,
+            start_images[lcd_num]
+        )
