085d991f66a9625d0c3fa7924c94954b264355a1
Kim Minseo <128784989+kmingseo@users.noreply.github.com>
2026-06-08 20:05:43 +0900

    update for fix

--- app/menu.py
+++ app/menu.py
@@ -9,7 +9,7 @@ MENU_IMAGE_DIR = Path("./static/images/menu")
 MENU_BUTTONS = [2, 4, 5, 6, 8]
 
 def show_start_page(display_manager):
-    images = load_images(MENU_IMAGE_DIR, "m")
+    images = load_images(MENU_IMAGE_DIR, "menu")
 
     for lcd_num in range(1, 10):
         display_manager.show_image(
